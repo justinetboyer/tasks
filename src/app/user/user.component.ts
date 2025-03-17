99322e060e779bef6a6fc5725923c2b00beb82ec
justinetboyer <justinetboyer@gmail.com>
2025-03-17 11:22:37 +0200

    revert

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,16 +8,16 @@ import { Component, computed, Input, signal, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({required:true}) avatar!: string ;
-  // @Input({required:true}) name!: string ;
+  @Input({required:true}) avatar!: string ;
+  @Input({required:true}) name!: string ;
 
-  avatar = input.required<string>();
-  name = input.required<string>();
-  imagePath = computed(() => `assets/users/${this.avatar()}`);
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // imagePath = computed(() => `assets/users/${this.avatar()}`);
 
-  // get imagePath(){
-  //   return `assets/users/${this.avatar}`;
-  // }
+  get imagePath(){
+    return `assets/users/${this.avatar}`;
+  }
 
   onUserSelected() {}
 }
